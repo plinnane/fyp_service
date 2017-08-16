2761b079bf64def6764cb27263e35732862fa790
Dennis Kuehn <dennis.kuehn@udo.edu>
2017-08-16 14:38:38 +0200

    Added a description to the script

--- deployDIMEApp.sh
+++ deployDIMEApp.sh
@@ -4,16 +4,19 @@ appWAR="${1}/target/dywa-app/app-presentation/target/app.war"
 dywaData="${HOME}/opt/wildfly-10.1.0.Final/standalone/data/"
 targetSystem="dennis@ls5vs005.cs.tu-dortmund.de:opt"
 
-if [ "x${2}" == "x" ]; then
-   echo "Using LS5 test machine account: ${targetSystem}"
-else
-   echo "Using specified target system: ${2}"
-   targetSystem=${2}
-fi
-if [ "x${1}" == "x" ]; then
-   echo "Please specify the repository of the DIME App"
+if [ "x${1}" == "x" ] || [ "${1}" == "help" ] ; then
+   echo "Use this script as follows:"
+   echo "   ${0} [projectPath] [targetSystem]"
+   echo "   projectPath  - specify the project that holds the generated target/dywa-app/app-presentation/target/app.war file"
+   echo "   targetSystem - (optional) the remote wildfly location. Default: ${targetSystem}"
    exit 0
 else
+   if [ "x${2}" == "x" ]; then
+      echo "Using LS5 test machine account: ${targetSystem}"
+   else
+      echo "Using specified target system: ${2}"
+      targetSystem=${2}
+   fi
    if [ -f ${appWAR} ]; then
       if [ -d ${dywaData} ]; then
          echo "Deploying ${appWAR} to ${targetSystem} ..."
