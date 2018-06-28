8dce92173694bfbfbc92ff2f06cdaa8cfbe1b9d2
labuser <labuser@lab-sbx-01.xct.com>
2018-06-28 06:53:14 -0400

    testing git and workspace

--- database-backup.sh
+++ database-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # backup
+#test git
 /usr/bin/java -cp /opt/wildfly-10.1.0.Final/modules/system/layers/base/com/h2database/h2/main/h2-1.3.173.jar org.h2.tools.Script -url "jdbc:h2:/opt/wildfly-10.1.0.Final/standalone/data/dywa;AUTO_SERVER=TRUE;MVCC=TRUE" -user sa -password sa -script /root/backup/fyp/fyp-db_$(date +"%Y-%m-%d_%H").sql
 # compress backup
 bzip2 /root/backup/fyp/fyp-db_$(date +"%Y-%m-%d_%H").sql
